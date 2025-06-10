8b78edb435e5cd3ad890fb2c0567096432bcecd3
IrasOksaras <g024c1054@g.neec.ac.jp>
2025-06-10 18:54:19 +0900

    chromiumのオプションを追加

--- home-manager/modules/web-browser/web-browser.nix
+++ home-manager/modules/web-browser/web-browser.nix
@@ -7,6 +7,14 @@
   programs.chromium = {
     enable = true;
     package = pkgs.chromium.override { enableWideVine = true; };
+    commandLineArgs = [
+      # vlukanSupport
+      "--enable-features=VaapiVideoDecodeLinuxGL,VaapiVideoEncoder,Vulkan,VulkanFromANGLE,DefaultANGLEVulkan,VaapiIgnoreDriverChecks,VaapiVideoDecoder,PlatformHEVCDecoderSupport,UseMultiPlaneFormatForHardwareVideo"
+      # waylandSupport
+      "--ozone-platform-hint=auto"
+      # ジェスチャーでの操作の有効化
+      "--enable-features=TouchpadOverscrollHistoryNavigation"
+    ];
   };
 
   home.file = {
