38082f61bc3abd40a179d748d33cced6d8fa5e36
IrasOksaras <g024c1054@g.neec.ac.jp>
2025-06-11 02:57:27 +0900

    ncspotのカラースキームを変更

--- home-manager/modules/ncspot/ncspot.nix
+++ home-manager/modules/ncspot/ncspot.nix
@@ -4,6 +4,27 @@
     enable = true;
     settings = {
       use_nerdfont = true;
+      flip_status_indicators = true;
+      volnorm = false;
+      volnorm_pregain = 12.5;
+      theme = {
+        # background = "#222436";
+        primary = "#C8D3F5";
+        secondary = "#828BB8";
+        title = "#82AAFF";
+        playing = "#FFC777";
+        playing_selected = "#FFB16C";
+        playing_bg = "#191B29";
+        highlight = "#FF966C";
+        highlight_bg = "#2F334D";
+        error = "#FF757F";
+        error_bg = "#222436";
+        statusbar = "#C099FF";
+        statusbar_progress = "#FFC777";
+        statusbar_bg = "#1E2030";
+        cmdline = "#C8D3F5";
+        cmdline_bg = "#1E2030";
+      };
     };
   };
 }
--- home-manager/modules/neovim/plugins/treesitter.nix
+++ home-manager/modules/neovim/plugins/treesitter.nix
@@ -2,15 +2,13 @@
   programs.nixvim.plugins = {
     treesitter = {
       enable = true;
+      settings = {
+        highlight.enable = true;
+        indent.enable = true;
+      };
     };
     treesitter-refactor = {
       enable = true;
-      highlightDefinitions = {
-        enable = true;
-      };
-      highlightCurrentScope = {
-        enable = true;
-      };
     };
   };
 }
