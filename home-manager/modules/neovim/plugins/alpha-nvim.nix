a4abb6f4bc17bee1041fbbc0bb52f25a78155afb
IrasOksaras <g024c1054@g.neec.ac.jp>
2025-07-16 19:35:04 +0900

    vimの起動画面の素敵性能が向上

--- home-manager/modules/neovim/plugins/alpha-nvim.nix
+++ home-manager/modules/neovim/plugins/alpha-nvim.nix
@@ -1,71 +1,76 @@
-{
+{pkgs, ...}: {
   programs.nixvim.plugins = {
     alpha = {
       enable = true;
-      # layout = [
-      #   {
-      #     type = "padding";
-      #     val = 2;
-      #   }
-      #   {
-      #     opts = {
-      #       hl = "Type";
-      #       position = "center";
-      #     };
-      #     type = "text";
-      #     val = [
-      #       "███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗"
-      #       "████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║"
-      #       "██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║"
-      #       "██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║"
-      #       "██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║"
-      #       "╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝"
-      #     ];
-      #   }
-      #   {
-      #     type = "padding";
-      #     val = 2;
-      #   }
-      #   {
-      #     type = "group";
-      #     val = [
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[ene]] end";
-      #         };
-      #         opts = {
-      #           shortcut = "n";
-      #         };
-      #         type = "button";
-      #         val = "  New file";
-      #       }
-      #       {
-      #         on_press = {
-      #           __raw = "function() vim.cmd[[qa]] end";
-      #         };
-      #         opts = {
-      #           shortcut = "q";
-      #         };
-      #         type = "button";
-      #         val = "󰈆 Quit Neovim";
-      #       }
-      #     ];
-      #   }
-      #   {
-      #     type = "padding";
-      #     val = 2;
-      #   }
-      #   {
-      #     opts = {
-      #       hl = "Keyword";
-      #       position = "center";
-      #     };
-      #     type = "text";
-      #     val = "Inspiring quote here.";
-      #   }
-      # ];
+      layout = [
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          opts = {
+            redraw = true;
+          };
+          type = "terminal";
+          command = ''
+echo -e "███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗\n████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║\n██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║\n██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║\n██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║\n╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝" | tte beams
+          '';
+          width = 44;
+          height = 6;
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "group";
+          val = [
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[ene]] end";
+              };
+              opts = {
+                shortcut = "n";
+              };
+              type = "button";
+              val = "  New file";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[qa]] end";
+              };
+              opts = {
+                shortcut = "q";
+              };
+              type = "button";
+              val = "󰈆 Quit Neovim";
+            }
+          ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          opts = {
+            hl = "Keyword";
+            position = "center";
+          };
+          type = "text";
+          val = "Inspiring quote here.";
+        }
+      ];
 
-      theme = "startify";
+      # theme = "startify";
     };
   };
+
+  home.packages = [
+    pkgs.figlet
+    pkgs.terminaltexteffects
+  ];
 }
