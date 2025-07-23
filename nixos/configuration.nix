c294e9d933c564541054d42e10725b61fa0e7257
IrasOksaras <g024c1054@g.neec.ac.jp>
2025-07-23 12:05:45 +0900

    りふぁくた

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -307,13 +307,15 @@
   };
 
   services.flatpak.enable = true;
+
   systemd.services.flatpak-repo = {
-  wantedBy = [ "multi-user.target" ];
-  path = [ pkgs.flatpak ];
-  script = ''
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  '';
-};
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   xdg = {
     # terminal-exec = {
     #   enable = true;
