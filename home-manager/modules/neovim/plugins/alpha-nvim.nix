879bac8da98ab360b323458301a4248fb4fdc011
IrasOksaras <g024c1054@g.neec.ac.jp>
2025-07-30 00:56:44 +0900

    vim起動時のアニメーションがランダムで変わるように変更

--- home-manager/modules/neovim/plugins/alpha-nvim.nix
+++ home-manager/modules/neovim/plugins/alpha-nvim.nix
@@ -1,77 +1,82 @@
-{config, pkgs, ...}: {
+{pkgs, ...}: {
 
-  programs.nixvim.plugins = {
-    alpha = {
-      enable = true;
-      layout = [
-        {
-          type = "padding";
-          val = 2;
-        }
-        {
-          opts = {
-            redraw = true;
-          };
-          type = "terminal";
-          command = "${config.xdg.dataHome}/nvim/scripts/startup.sh";
-          width = 54;
-          height = 11;
-        }
-        {
-          type = "padding";
-          val = 2;
-        }
-        {
-          type = "padding";
-          val = 2;
-        }
-        {
-          type = "group";
-          val = [
-            {
-              on_press = {
-                __raw = "function() vim.cmd[[ene]] end";
-              };
-              opts = {
-                shortcut = "n";
-              };
-              type = "button";
-              val = " New file";
-            }
-            {
-              on_press = {
-                __raw = "function() vim.cmd[[qa]] end";
-              };
-              opts = {
-                shortcut = "q";
-              };
-              type = "button";
-              val = "󰈆 Quit Neovim";
-            }
-          ];
-        }
-        {
-          type = "padding";
-          val = 2;
-        }
-        {
-          opts = {
-            hl = "Keyword";
-            position = "center";
-          };
-          type = "text";
-          val = "Inspiring quote here.";
-        }
-      ];
+  programs.nixvim  = {
+    plugins = {
+      alpha = {
+        enable = true;
+        # layout = [
+        #   {
+        #     type = "padding";
+        #     val = 4;
+        #   }
+        #   {
+        #     opts = {
+        #       redraw = true;
+        #     };
+        #     type = "terminal";
+        #     command = "${config.xdg.dataHome}/nvim/scripts/startup.sh";
+        #     width = 54;
+        #     height = 11;
+        #   }
+        #   {
+        #     type = "padding";
+        #     val = 2;
+        #   }
+        #   {
+        #     opt = {
+        #       "inherit" = {
+        #         position = "center";
+        #       };
+        #     };
+        #     type = "group";
+        #     val = [
+        #       {
+        #         on_press = {
+        #           __raw = "function() vim.cmd[[ene]] end";
+        #         };
+        #         opts = {
+        #           shortcut = "n";
+        #           position = "center";
+        #         };
+        #         type = "button";
+        #         val = " New file";
+        #       }
+        #       {
+        #         on_press = {
+        #           __raw = "function() vim.cmd[[qa]] end";
+        #         };
+        #         opts = {
+        #           # cursor = 2;
+        #           shortcut = "q";
+        #           position = "center";
+        #         };
+        #         type = "button";
+        #         val = "󰈆  > Quit NVIM";
+        #       }
+        #     ];
+        #   }
+        #   {
+        #     type = "padding";
+        #     val = 2;
+        #   }
+        #   {
+        #     opts = {
+        #       hl = "Keyword";
+        #       position = "center";
+        #     };
+        #     type = "text";
+        #     val = "Inspiring quote here.";
+        #   }
+        # ];
 
-      # theme = "startify";
+        theme = "theta";
+      };
     };
-  };
 
-  home.packages = [
-    pkgs.figlet
-    pkgs.terminaltexteffects
-  ];
+    extraPackages = [
+      pkgs.terminaltexteffects
+    ];
+  };
 
   xdg.dataFile = {
     "nvim/scripts/startup.sh" = {
