ffba092aa009aa953d43d69f51cde7727dbc57d2
IrasOksaras <g024c1054@g.neec.ac.jp>
2025-09-04 11:15:46 +0900

    remote-nvim二設定追加

--- home-manager/modules/neovim/plugins/remote-nvim.nix
+++ home-manager/modules/neovim/plugins/remote-nvim.nix
@@ -3,6 +3,19 @@
     plugins = {
       remote-nvim = {
         enable = true;
+        settings = {
+          ssh_config = {
+            ssh_binary = "ssh";
+            scp_binary = "scp";
+            ssh_config_file_paths = [ "$HOME/.ssh/config" ];
+            ssh_prompts = {
+              match = "Enter passphrese for key";
+              type = "secret";
+              value_type = "static";
+              value = "";
+            };
+          };
+        };
       };
     };
   };
