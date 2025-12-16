5b72929d94f32abc7bf385505baa7c59d6b9cce8
sunao1222 <g024c1054@g.neec.ac.jp>
2025-12-16 15:12:11 +0900

    fix:sshConfigを変更

--- home-manager/modules/ssh/ssh.nix
+++ home-manager/modules/ssh/ssh.nix
@@ -2,6 +2,13 @@
   programs = {
     ssh = {
       enable = true;
+      matchBlocks = {
+        "*" = {
+          forwardAgent = true;
+          compression = true;
+          addKeysToAgent = "yes";
+        };
+      };
     };
   };
 
