064c99af796bbb4a3b75f0c6a27a6d114dfe9b8e
sunao1222 <g024c1054@g.neec.ac.jp>
2025-12-17 13:16:58 +0900

    add:weztermのzsh統合を追加

--- home-manager/modules/wezterm/wezterm.nix
+++ home-manager/modules/wezterm/wezterm.nix
@@ -3,6 +3,7 @@
   programs.wezterm = {
     enable = true;
     # package = inputs.wezterm.packages.${pkgs.system}.default;
+    enableZshIntegration = true;
     extraConfig = builtins.readFile ./wezterm.lua;
   };
 }
