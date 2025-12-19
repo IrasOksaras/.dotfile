19a8a841d14aebe129ab30b9dd4514671fbf2a4f
sunao1222 <g024c1054@g.neec.ac.jp>
2025-12-19 15:00:17 +0900

    fix:sshDefaultConfigを使用しないよう変更

--- home-manager/modules/ssh/ssh.nix
+++ home-manager/modules/ssh/ssh.nix
@@ -2,11 +2,19 @@
   programs = {
     ssh = {
       enable = true;
+      enableDefaultConfig = false;
       matchBlocks = {
         "*" = {
           forwardAgent = true;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
           compression = true;
           addKeysToAgent = "yes";
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
         };
       };
     };
